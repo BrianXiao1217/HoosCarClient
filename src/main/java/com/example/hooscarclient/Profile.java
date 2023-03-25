ff9c0adee28d99533565be399a609145fb8e4b6f
Brian Xiao <qhu2fg@virginia.edu>
2023-03-25 13:45:49 -0400

    made Profile Class

---
+++ src/main/java/com/example/hooscarclient/Profile.java
@@ -0,0 +1,57 @@
+package com.example.hooscarclient;
+import java.util.*;
+
+public class Profile {
+    private String phoneNumber;
+    private String username;
+    private String password;
+    private List<String> carPools;
+    private List<String> hostedPools;
+    public Profile(String phone, String user, String pass){
+        phoneNumber = phone;
+        username = user;
+        password = pass;
+        carPools = new ArrayList<String>();
+        hostedPools = new ArrayList<String>();
+    }
+    public Profile(){
+        this("","","");
+    }
+    private void setPhoneNumber(String phone){
+        phoneNumber = phone;
+    }
+    public void addPool(String id){
+        carPools.add(id);
+    }
+    public List<String> getPools(){
+        return carPools;
+    }
+    public boolean removePools(String id){
+        return carPools.remove(id);
+    }
+    public void addHosted(String id){
+        hostedPools.add(id);
+    }
+    public boolean removeHosted(String id){
+        return hostedPools.remove(id);
+    }
+    public List<String> getHostedPools(){
+        return hostedPools;
+    }
+    private String getPhoneNumber(){
+        return phoneNumber;
+    }
+    private void setUsername(String user){
+        username = user;
+    }
+    private String getUsername(){
+        return username;
+    }
+    private void setPassword(String pass){
+        password = pass;
+    }
+    private String getPassword(){
+        return password;
+    }
+
+}
